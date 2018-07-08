c65a24a7afa9ca99b924d079eae5af46586867a1
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-07-08 18:33:05 +0200

    file (lib) update capabilities added

--- lib/kamaze/docker_hosts/file.rb
+++ lib/kamaze/docker_hosts/file.rb
@@ -6,40 +6,87 @@
 # This is free software: you are free to change and redistribute it.
 # There is NO WARRANTY, to the extent permitted by law.
 
-require_relative '../docker_hosts'
-require 'hosts'
+require_relative 'base_file'
 
-# @see https://github.com/aef/hosts
-class Kamaze::DockerHosts::File < Aef::Hosts::File
-  autoload :Pathname, 'pathname'
-
-  # Initializes a file.
+# Hosts file, with update capabilities.
+#
+# Sample of use:
+#
+# ```ruby
+# hosts = File.new('/etc/hosts')
+# hosts.update!(Network.new)
+# puts hosts
+# ```
+class Kamaze::DockerHosts::File < Kamaze::DockerHosts::BaseFile
+  # Get networks, indexed by names.
   #
-  # @param [String|Pathname|nil] path path to the hosts file
-  def initialize(path = '/etc/hosts')
-    reset
-    self.path = path
-    autoread
+  # @return [nil|Hash{Symbol => Kamaze::DockerHosts::Network}]
+  attr_reader :networks
+
+  # Get a new section populated with network records.
+  #
+  # @return [Aef::Hosts::Section]
+  def section(name)
+    begin
+      network = self.networks.fetch(name.to_sym).to_h
+    rescue KeyError
+      raise ArgumentError, "#{name} not in #{networks.keys}"
+    end
+
+    Hosts::Section.new(name, elements: []).tap do |section|
+      network.to_h.each do |host, records|
+        section.elements.push(Aef::Hosts::Entry.new(records.fetch(0), host))
+      end
+    end
   end
 
-  class << self
-    # Parses a hosts file given as String.
-    #
-    # @param [String] data a String representation of the hosts file
-    # @return [Aef::Hosts::File] a file
-    def parse(data)
-      new(nil).parse(data)
+  def update!(*args)
+    self.elements = self.update(*args).elements
+
+    self
+  end
+
+  # Update hosts with given network.
+  #
+  # @param [Kamaze::DockerHosts::Network] network
+  # @param [String|Symbol] name
+  # @return [self]
+  def update(network, name = :containers)
+    self.networks = self.networks.to_h.merge(name.to_sym => network)
+    self.updating = name.to_sym
+
+    update_on(self.class.new(*[path].compact)).tap do
+      self.updating = nil
     end
   end
 
   protected
 
-  # @return [self]
-  def autoread
-    unless path.nil?
-      read if self.path.file? and self.path.readable?
-    end
+  # @type [Hash]
+  attr_writer :networks
 
-    self
+  # Used during update.
+  #
+  # This attribute has a very short lifecycle, and SHOULD be nullified
+  # when update is considered as done.
+  #
+  # @type [Symbol|nil]
+  attr_accessor :updating
+
+  # Applies update on given instance.
+  #
+  # @param [self] instance
+  # @return [self]
+  def update_on(instance)
+    instance.tap do |hosts|
+      hosts.elements.delete_if do |elem|
+        elem.is_a?(Aef::Hosts::Section) and elem.name == self.updating.to_s
+      end.tap do |elements|
+        loop do
+          break unless hosts.elements.last.is_a?(Aef::Hosts::EmptyElement)
+          hosts.elements.delete_at(-1)
+        end
+      end.push(section(updating))
+    end
   end
 end
