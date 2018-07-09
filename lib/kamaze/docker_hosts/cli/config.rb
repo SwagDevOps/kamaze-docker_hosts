61056e62abb2c52acf816098f67457e2d8069f70
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-07-09 11:58:14 +0200

    config (cli) introduced

---
+++ lib/kamaze/docker_hosts/cli/config.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+require_relative '../cli'
+require 'figgy'
+
+# Provide access to configuration files.
+#
+# Recognize missing methods and go look them up as a configuration key.
+#
+# To create a new instance, use ``Figgy.build``:
+#
+# ```ruby
+# config = Figgy.build do |c|
+#   c.root = '/path/to/my/configs'
+# end
+# config.foo.bar # read from /path/to/my/configs/foo.yml
+# ```
+#
+# @see https://github.com/pd/figgy
+class Kamaze::DockerHosts::Cli::Config < Figgy
+  autoload :Pathname, 'pathname'
+
+  class << self
+    # @yield [Figgy::Configuration] an object to set things up with
+    # @return [Figgy] a Figgy instance using the configuration
+    def build
+      config = Figgy::Configuration.new.tap do |c|
+        c.root = roots.fetch(0)
+        c.add_root(roots.fetch(1))
+
+        # config.foo is read from etc/foo.yml
+        c.define_overlay :default, nil
+      end
+
+      yield(config) if block_given?
+      new(config)
+    end
+
+    # Get name of current running program.
+    #
+    # @return [String]
+    def progname
+      $PROGRAM_NAME
+    end
+
+    # @return [Array<Pathname>]
+    def roots
+      [
+        Pathname.new(__dir__).join('config'),
+        Pathname.new('/etc').join($PROGRAM_NAME),
+      ]
+    end
+  end
+end
