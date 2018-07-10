2510584034ae22f0ca1851effd2c5a256d123421
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-07-10 19:12:50 +0200

    commands/version (cli) rewrite

--- lib/kamaze/docker_hosts/cli/commands/version.rb
+++ lib/kamaze/docker_hosts/cli/commands/version.rb
@@ -3,26 +3,34 @@
 require_relative '../commands'
 
 # Version
-class Kamaze::DockerHosts::Cli::Commands::Version < Hanami::CLI::Command
-  desc 'Print version'
+class Kamaze::DockerHosts::Cli
+  # Display version.
+  class Commands::Version < Command
+    register 'version', aliases: ['--version']
+    desc 'Print version'
 
-  def call(*)
-    ["#{progname} #{version}",
-     nil,
-     version.license_header].join("\n").tap do |str|
-      $stdout.puts(str)
+    def call(*)
+      ["#{progname} #{version}",
+       nil,
+       version.license_header].join("\n").tap do |str|
+        $stdout.puts(str)
+      end
     end
-  end
 
-  protected
+    protected
 
-  # @return [Kamaze::DockerHosts::VERSION]
-  def version
-    Kamaze::DockerHosts::VERSION
-  end
+    # Get version.
+    #
+    # @return [Kamaze::DockerHosts::VERSION]
+    def version
+      Kamaze::DockerHosts::VERSION
+    end
 
-  # @return [String]
-  def progname
-    $PROGRAM_NAME
+    # Get progname.
+    #
+    # @return [String]
+    def progname
+      $PROGRAM_NAME
+    end
   end
 end
