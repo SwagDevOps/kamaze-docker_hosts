a16128d3c9ce926c1231a842cc49b1fa2f6b0d76
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-07-10 19:16:08 +0200

    commands/hosts (cli) cleanup + rewrite

--- lib/kamaze/docker_hosts/cli/commands/hosts.rb
+++ lib/kamaze/docker_hosts/cli/commands/hosts.rb
@@ -1,46 +1,31 @@
 # frozen_string_literal: true
 
 require_relative '../commands'
-require_relative '../../file'
 
 class Kamaze::DockerHosts::Cli
-  # Display network status
+  # Display hosts
   class Commands::Hosts < Command
+    register 'hosts'
     enable_network
     desc 'Display hosts'
 
     def call(**options)
-      hosts = Kamaze::DockerHosts::File.new.update!(network)
+      configure(options)
 
-      $stdout.puts(hosts)
+      file.update!(network).tap do |content|
+        $stdout.puts(content)
+      end
     end
 
     protected
 
-    # Prepare network rendering.
+    # Get file.
     #
-    # @param [Kamaze::DockerHosts::Network] network
-    # @return [Array]
-    def prepare(network)
-      network = network.to_a.map(&:flatten)
-      max = network.map(&:size).max
+    # @return [Kamaze::DockerHosts::File]
+    def file
+      require_relative '../../file'
 
-      network.map { |row| row.push(*([nil] * (max - row.size))) }
-    end
-
-    def render(network, raw = false) # rubocop:disable Metrics/MethodLength
-      return if network.empty?
-
-      Terminal::Table.new do |table|
-        table.rows = prepare(network)
-        table.style = {
-          border_top: false,
-          border_bottom: false,
-          border_y: '',
-          padding_left: 0,
-          padding_right: 4,
-        }.public_send(raw ? :to_h : :clear)
-      end
+      Kamaze::DockerHosts::File.new
     end
   end
 end
